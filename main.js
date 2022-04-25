105d56e60a4f1f83ed2aca2f0b5bc4b10007e26a
mhjarvis <markus.h.jarvis@gmail.com>
2022-04-25 09:00:58 -0500

    Create computerPlay() to randomize computer's pick

--- main.js
+++ main.js
@@ -1,1 +1,31 @@
-console.log("Hello there!");
+
+function computerPlay () {              //function randomly picks and returns computers value
+    
+    let randomNum = Math.floor(Math.random() * 3 + 1);
+    
+    if(randomNum == 1) {
+        return "Rock";
+    }
+    if(randomNum == 2) {
+        return "Paper";
+    }
+    if(randomNum == 3) {
+        return "Scissors";
+    }
+}
+
+
+console.log(computerPlay());
+console.log(computerPlay());
+console.log(computerPlay());
+console.log(computerPlay());
+console.log(computerPlay());
+console.log(computerPlay());
+console.log(computerPlay());
+console.log(computerPlay());
+console.log(computerPlay());
+console.log(computerPlay());
+console.log(computerPlay());
+console.log(computerPlay());
+console.log(computerPlay());
+console.log(computerPlay());
