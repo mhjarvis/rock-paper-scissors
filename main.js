e916221a3f49be761342cc65eeb99bea6ccdbd53
mhjarvis <markus.h.jarvis@gmail.com>
2022-04-25 09:45:14 -0500

    Create self-invoking function to welcome player, get player choice

--- main.js
+++ main.js
@@ -1,3 +1,24 @@
+(function() {                           //welcome function/prompt to start game
+
+    console.log("Welcome to Tic-Tac-Toe");
+    console.log(".......................");
+    
+    let answer = false;
+
+    while(answer == false) {
+        let playerChoice = prompt("Pick one - Rock, Paper, or Scissors: ");
+        playerChoice = playerChoice.toUpperCase();
+        console.log(playerChoice);
+
+        if(playerChoice == "ROCK" || playerChoice == "PAPER" || playerChoice == "SCISSORS") {
+            answer = true;
+            return playerChoice;
+        }
+    }
+    
+
+})();
+
 
 function computerPlay () {              //function randomly picks and returns computers value
     
@@ -15,9 +36,6 @@ function computerPlay () {              //function randomly picks and returns co
 }
 
 
-console.log(computerPlay());
-console.log(computerPlay());
-console.log(computerPlay());
 console.log(computerPlay());
 console.log(computerPlay());
 console.log(computerPlay());
